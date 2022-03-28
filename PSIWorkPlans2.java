b9b04072e904c066607af1ea9565e7fb8e4f8bf7
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-03-28 16:34:24 -0400

    Add files via upload

--- PSIWorkPlans2.java
+++ PSIWorkPlans2.java
@@ -3,13 +3,35 @@ import java.util.Scanner;
  * 2. Create a method that finds the smallest value out of 3 values
  * 3. Create a method that calculates the average
  * 4. Create a loop that counts the number of times the same number appears more than once
+ * 5. Create a method that determines if an number entered is even or odd
+ * 6. Create a method that determines if a number entered is a multiple of 2 or 5
+ * 7. Create a loop that displays the lowest grade and highest from the 10 grades entered
  */
 
 public class PSIWorkPlans2 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		/*System.out.println(Largest(30, 30, 30));
 		System.out.println(Largest(30, 30, 30));
-		System.out.println(Largest(30, 30, 30));
+		System.out.print(EvenOdd(11));*/
+		
+		int grade = 0;
+		int highest = 0;
+		int lowest = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Enter grade " + i + ":");
+			grade = input.nextInt();
+			if (grade > highest) {
+				highest = grade;
+			}
+			if (grade < lowest) {
+				lowest = grade;
+			}
+		}
+		System.out.println("Highest Grade is: "+ highest);
+		System.out.println("Lowest Grade is: "+ lowest);
 		
 	}
 	public static int Largest(int num1, int num2, int num3) {
@@ -33,6 +55,14 @@ public class PSIWorkPlans2 {
 		}
 		else {
 			return num3;
+		}
+	}
+	public static String EvenOdd(int num1) {
+		if (num1 % 2 == 0) {
+			return "even";
+		}
+		else {
+			return "odd";
 		}
 	}
 		
